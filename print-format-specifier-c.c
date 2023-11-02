e74960bf9f3af740208c53ef57015df6e33f4377
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-11-02 14:59:25 -0500

    Create print-format-specifier-c.c
    Loaded starter template

---
+++ print-format-specifier-c.c
@@ -0,0 +1,41 @@
+void thennameofourcustomfunction(const char *format, ...)
+{
+
+    /* Starting Variables */
+    char *str = NULL;
+    int i;
+
+
+    /* Check If Format Exists */
+
+
+
+    /* Check For Null Byte*/
+
+
+
+    /* Check For % To Initiate Format */
+
+
+
+    /* Check For Format Specifier */
+
+
+
+    /* What If No %? */
+
+
+
+    /* Print String */
+
+
+
+
+
+    /* Iterate */
+
+        _putchar('\n');
+        format++;
+
+    }
+}
