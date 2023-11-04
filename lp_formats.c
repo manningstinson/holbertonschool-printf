6cd94c2c8fbfa608ad6f9e48a3936d620cd07946
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2023-11-04 18:58:16 -0500

    Update lp_formats.c
    Updated

--- lp_formats.c
+++ lp_formats.c
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-#include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-
-/**
- * print_percent_sign - Print a percent sign when encountering '%%' in a format string
- * @args: Unused va_list argument
- *
- * Return: Number of characters printed (1)
- */
-int lp_print_percent_sign(__attribute__((unused)) va_list args)
-{
-    putchar('%');
-    return (1);
-}
-
-/**
- * print_ignore - Ignores the provided argument and returns 0
- * @args: Unused va_list argument
- *
- * Return: 0
- */
-int lp_print_ignore(__attribute__((unused)) va_list args)
-{
-    return 0;
-}
-
-/**
- * print_special - Handles special formatting and prints a percent sign and a character
- * @args: Unused va_list argument
- *
- * Return: 2 (the length of "%c")
- */
-int lp_print_special(__attribute__((unused)) va_list args)
-{
-    return 2;
-}
-=======
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
@@ -77,4 +37,3 @@ int catch_all(__attribute__((unused)) va_list args)
 {
 	return (2);
 }
->>>>>>> 2e5f58451f99adbb89439e2845c81dd4318acc22
