4abdbd175ea238643c4877cd653c4ee667cfa5f1
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2023-11-04 18:59:56 -0500

    Update main.h
    Updated

--- main.h
+++ main.h
@@ -1,26 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-<<<<<<< HEAD
 #ifndef HEADER
 #define HEADER
-=======
->>>>>>> 2e5f58451f99adbb89439e2845c81dd4318acc22
 #include <stdarg.h>
 
 typedef struct formats
 {
-<<<<<<< HEAD
 	char spec;
 	int (*print_func)(va_list);
-} 
-=======
-        char spec;
-        int (*print_func)(va_list);
-}
->>>>>>> 2e5f58451f99adbb89439e2845c81dd4318acc22
-
-formats_t;
+} formats_t;
 
 int _printf(const char *, ...);
 int (*get_format(char))(va_list);
@@ -33,6 +19,5 @@ int catch_all(va_list);
 int _putchar(char);
 int putint(int);
 
+
 #endif
-
-
