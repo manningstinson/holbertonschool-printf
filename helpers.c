1d7d6a65ee416fbcfe8a9dd2c305cc0dda85d9a6
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2023-11-04 19:55:51 -0500

    Update helpers.c

--- helpers.c
+++ helpers.c
@@ -1,1 +1,26 @@
+#include "main.h"
+#include <unistd.h>
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * putint - writes the integer i to stdout
+ * @i: the integer to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int putint(int i)
+{
+	return (write(1, &i, 4));
+}
