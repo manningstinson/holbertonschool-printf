de734127179c62220ef84d539e4a87ed4643331e
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2023-11-04 19:56:58 -0500

    Update print_char.c

--- print_char.c
+++ print_char.c
@@ -1,1 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
 
+/**
+ * print_char - prints a character
+ * @args: arguments to format data type
+ * Return: Number of characters printed (should be 1)
+ */
+
+int print_char(va_list args)
+{
+	char ch = va_arg(args, int);
+
+	putchar(ch);
+	return (1);
+}
