42c6bf201113518bf9080b8344b152a476819f96
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2023-11-05 12:07:38 -0600

    Update and rename main.h to lp_main.h

---
+++ lp_main.h
@@ -0,0 +1,20 @@
+#ifndef LP_MAIN_H
+#define LP_MAIN_H
+#include <stddef.h>
+#include <stdarg.h>
+
+typedef struct specifier
+{
+        char specifier;
+        int (*print_func)(va_list args);
+} specifier_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_int(va_list args);
+specifier_t get_specifier(char c);
+
+#endif
