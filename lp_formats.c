651fd72db82602570c8eea4132fb54175e4a7382
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2023-11-05 12:05:17 -0600

    Update and rename formats.c to lp_formats.c

---
+++ lp_formats.c
@@ -0,0 +1,43 @@
+#include <stdarg.h>
+#include "lp_main.h"
+#include <stddef.h>
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+
+    if (format == NULL)
+        return (-1);
+
+    va_start(args, format);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            specifier_t specifier;
+            int (*specifier_func)(va_list);
+
+            format++;
+            specifier = get_specifier(*format);
+            specifier_func = specifier.print_func;
+
+            if (specifier_func)
+                count += specifier_func(args);
+            else if (*format)
+            {
+                count += _putchar('%');
+                count += _putchar(*format);
+            }
+            else
+                return (-1);
+        }
+        else
+        {
+            count += _putchar(*format);
+        }
+        format++;
+    }
+    va_end(args);
+    return (count);
+}
