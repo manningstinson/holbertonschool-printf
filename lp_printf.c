e25b854432fd36176fb32d1b2fa0665838351b2e
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2023-11-04 19:01:00 -0500

    Update lp_printf.c
    updated

--- lp_printf.c
+++ lp_printf.c
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-#include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
-
-int _printf(const char *format, ...)
-{
-    int count = 0;
-    int func;
-    int (*print_func)(va_list);
-    va_list args;
-
-    if (format)
-    {
-        va_start(args, format);
-        while (*format != '\0')
-        {
-            if (*format == '%')
-            {
-                format++;
-                print_func = get_format(*format);
-                if (print_func != NULL)
-                {
-                    func = print_func(args);
-                    if (func == -1)
-                        return (-1);
-                    count += func;
-                }
-            }
-            else
-            {
-                putchar(*format);
-                count++;
-            }
-            format++;
-        }
-        va_end(args);
-        return count;
-    }
-    return (-1);
-}
-=======
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
@@ -87,4 +45,3 @@ int _printf(const char *format, ...)
 	}
 	return (-1);
 }
->>>>>>> 2e5f58451f99adbb89439e2845c81dd4318acc22
