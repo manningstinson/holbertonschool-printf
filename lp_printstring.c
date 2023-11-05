63a0b12006b8753274ab75d0c65f5edbd5416477
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2023-11-04 19:04:52 -0500

    Update lp_printstring.c

--- lp_printstring.c
+++ lp_printstring.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
@@ -20,26 +19,3 @@ int print_string(va_list args)
 	fputs(str, stdout);
 	return (strlen(str));
 }
-=======
-#include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
-
-/**
- * print_string - prints a string
- * @args: arguments to format data type
- * Return: Number of characters printed (null byte excluded)
- */
-int print_string(va_list args)
-{
-	char *str = va_arg(args, char*);
-
-	if (str == NULL)
-	{
-		str = "(null)";
-	}
-	fputs(str, stdout);
-	return (strlen(str));
-}
->>>>>>> 2e5f58451f99adbb89439e2845c81dd4318acc22
