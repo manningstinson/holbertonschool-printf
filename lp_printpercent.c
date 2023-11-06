2c3ee865b306155adef2bc013b657c20d668a768
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-11-06 09:27:06 -0600

    Update lp_printpercent.c
    updated betty styles

--- lp_printpercent.c
+++ lp_printpercent.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * print_percent - Prints a percent symbol to the standard output.
+ * @args: A va_list (not used in this function).
+ *
+ * Return: The number of characters printed (1).
+ */
+
 int print_percent(va_list args)
 {
     (void)args;
